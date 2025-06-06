a8364959ef1f73fe9acdbb2c6c06b7b2b11d7e9b
Amirreza Riahi <amirreza.riahi3311@gmail.com>
2025-06-06 13:16:31 +0330

    Coding Authorization

---
+++ App/Validation/Authorization.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Validation;
+session_start();
+class Authorization
+{
+    private $user;
+    public static function getUserByUsername($username)
+    {
+        global $database;
+        $result = $database->get("users", "*", [
+            "name" => $username
+        ]);
+        return $result ?: null;
+    }
+
+    public static function login($username, $password)
+    {
+        $user = self::getUserByUsername($username);
+        if ($user === null)
+            return "User Not Found";
+        if ($user["password"] === $password) {
+            $_SESSION["login"] = $user;
+            return true;
+        }
+        return "Username Or Password Is Incorrect !!!";
+    }
+    public static function curUserId()
+    {
+        return $_SESSION["login"]["id"] ?? null;
+    }
+    public static function curUser()
+    {
+        return $_SESSION["login"] ?? null;
+    }
+    public static function isLogged()
+    {
+        return isset($_SESSION["login"]);
+    }
+}

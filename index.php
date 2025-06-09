7a605e252d0c1a31aa4d7c886c6a071463fca6ff
Amirreza Riahi <amirreza.riahi3311@gmail.com>
2025-06-09 09:50:42 +0330

    add AccDetails

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
-
+use App\Utilities\AccDetails;
 use App\Validation\Authorization;
 include "./bootstrap/init.php";
 if (isset($_GET["logout"]) && is_numeric($_GET["logout"]))
     Authorization::logout();
+$accDetails = new AccDetails();
 include BASE_PATH . "/Template/home.php";
